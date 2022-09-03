3cbb34c81325ee4debce07470a9afbb1d6eb84cc
RobertoConrado <robertoconradorc@gmail.com>
2022-09-03 20:15:26 -0300

    retorno_v

---
+++ index.js
@@ -0,0 +1,9 @@
+
+var condicional = (5 - 10) && (5 + 5)
+                    //-5 true   //10 verdadeiro
+if(condicional) {
+    console.log("verdadeiro", condicional);//verdadeiro 10
+    //Sempre retorna a primeira falso ou a última verdadeiro
+}else {
+    console.log("falso");
+}
