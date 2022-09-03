ce1f6d80fa5a6dc75b9d79358601d52a074425f0
RobertoConrado <robertoconradorc@gmail.com>
2022-09-03 20:38:53 -0300

    expressao

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+//!important
 var condicional = (5 - 10) && (5 + 5)
                     //-5 true   //10 verdadeiro
 if(condicional) {
@@ -7,3 +7,19 @@ if(condicional) {
 }else {
     console.log("falso");
 }
+
+//Exercício
+//Verifique se sua idade é maior do que a de algum parente.
+let minhaIdade = 20; let idadeParente = 20;
+if(minhaIdade > idadeParente) {
+    console.log("Sou mais velho!");
+}else if (minhaIdade < idadeParente) {
+    console.log("Sou mais novo!");
+}else {
+    console.log("Somos da mesma idade!");
+}
+
+// Qual valor é retornado da seguinte expressão?
+var expressao = (5 - 2) && (5 - ' ') && (5 - 2);
+               //  3           5           3
+//Sempre que a expressao tem _&&_ retorna a primeira falso ou a última verdadeiro               
