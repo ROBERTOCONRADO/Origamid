40ffb4b2679a0b5b0e376404718476ee1b56dae1
RobertoConrado <robertoconradorc@gmail.com>
2022-09-05 23:04:16 -0300

    acessibilidade

--- AnimaisFantasticos/dimensoes&distancias.js
+++ AnimaisFantasticos/dimensoes&distancias.js
@@ -4,6 +4,28 @@ const imgTop = img.offsetTop;
 console.log(imgTop);
 
 //Retorne a soma e a largura de todas as imagens
-img.
-//Verifique se os links da página possuem o mínimo recomendado para as relas utilizads com o dedo. (48px/48px de acordo com o google
+function somaImagens() {
+    const imagens = document.querySelectorAll('img');
+    let soma = 0;
+    imagens.forEach((image) => {
+        soma += image.offsetWidth;
+    });
+    console.log(soma);
+}
+window.onload = () => {
+    somaImagens();
+};
+
+//Verifique se os links da página possuem o mínimo recomendado para as telas utilizads com o dedo. (48px/48px de acordo com o google)
+const alink = document.querySelectorAll('a');
+alink.forEach((link) => {
+    const linkWidth = link.offsetWidth;
+    const linkHeight = link.offsetHeight;
+    if(linkWidth >= 48 && linkHeight >= 48) {
+        console.log(link, "Posui boa acassibilidade!");
+    }else{
+        console.log(link, "Não posui boa acessibilidade!");
+    }
+});
+
 //Se o browser for menor que 720px, adicione a classe menu-mobile ao menu
