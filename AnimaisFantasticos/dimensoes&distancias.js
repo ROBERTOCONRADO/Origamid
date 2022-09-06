2b7230a1689c44159211eb64a1fa04d2628289f7
RobertoConrado <robertoconradorc@gmail.com>
2022-09-05 22:38:45 -0300

    dimensoes

---
+++ AnimaisFantasticos/dimensoes&distancias.js
@@ -0,0 +1,9 @@
+//Verifique a distância da primeira imagem em relação ao topo da página
+const img = document.querySelector('img');
+const imgTop = img.offsetTop;
+console.log(imgTop);
+
+//Retorne a soma e a largura de todas as imagens
+img.
+//Verifique se os links da página possuem o mínimo recomendado para as relas utilizads com o dedo. (48px/48px de acordo com o google
+//Se o browser for menor que 720px, adicione a classe menu-mobile ao menu
