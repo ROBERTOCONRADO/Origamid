1295c45041765e71671ae0320bf4e571da64370d
RobertoConrado <robertoconradorc@gmail.com>
2022-09-07 15:53:32 -0300

    addClass&removeClass

--- ConstructorFunction/script.js
+++ ConstructorFunction/script.js
@@ -14,10 +14,28 @@ function Pessoa(nome, idade) {
     }
 }
 
-
-//Crie 3 pessoas, Joao - 20 anos, Maria - 2 anos, bruno - 22 anos
+//Crie 3 pessoas, John - 20 anos, Maria - 21 anos, Bruna - 22 anos
+const john = new Pessoa('Joao', 20);
+const maria = new Pessoa('Maria', 21);
+const bruna = new Pessoa('Bruna', 22);
 
 //Crie uma Constructor Function Dom para manipulação de linhas de elemento do dom. Deve conter os seguintespropriedades e métodos:
 //elements, retorna NodeList com elementos selecionados,
 //addClass(classe), adiciona a classe a todos os elementos,
 //removeClass(classe), remove a classe de todos os elementos;
+function Dom(seletor) {
+    const list = document.querySelectorAll(seletor);
+    this.elements = list;
+    this.addClass = function(classe) {
+        list.forEach((element) => {
+            element.classList.add(classe);
+        })
+    };
+    this.removeClass = function(classe) {
+        list.forEach((element) => {
+            element.classList.remove(classe);
+        })
+    };
+}
+const list = new Dom('li');
+const ul = new Dom('ul');
