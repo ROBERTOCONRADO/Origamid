f56e516b1ce8e2ce31678e20f44cd02d8fd56880
RobertoConrado <robertoconradorc@gmail.com>
2022-09-06 21:02:07 -0300

    accordion

--- AnimaisFantasticos/navagacao-por-tab.js
+++ AnimaisFantasticos/navagacao-por-tab.js
@@ -1,14 +1,30 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-const tabContent = document.querySelectorAll('.js-tabcontent section');
+function initTabNav() {
+    const tabMenu = document.querySelectorAll('.js-tabmenu li');
+    const tabContent = document.querySelectorAll('.js-tabcontent section');
 
-function ativoTab(index) {
-    tabContent.forEach((section) => {
-        section.classList.remove('ativo');
-    });
-    tabContent[index].classList.add('ativo');
+    if(tabMenu.length && tabContent.length) {
+        tabContent[0].classList.add('ativo');
+        function ativoTab(index) {
+            tabContent.forEach((section) => {
+                section.classList.remove('ativo');
+            });
+            tabContent[index].classList.add('ativo');
+        }
+        tabMenu.forEach((itemMenu, index) => {
+            itemMenu.addEventListener('click', () => {
+                ativoTab(index);
+            });
+        });
+    }
 }
-tabMenu.forEach((itemMenu, index) => {
-    itemMenu.addEventListener('click', () => {
-        ativoTab(index);
-    });
+initTabNav();
+
+const acoordionList = document.querySelectorAll('.js-acordion dt')
+function activeAcoordion() {
+    this.classList.add('active');
+    this.nextElementSibling.classList.add('active');
+}
+acoordionList.forEach((item) => {
+ item.addEventListener('click', activeAcoordion);
 });
+
