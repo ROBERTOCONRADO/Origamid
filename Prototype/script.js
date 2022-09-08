0d9b08728a039f41ebcc29cac8ecb455411d579c
RobertoConrado <robertoconradorc@gmail.com>
2022-09-07 22:16:23 -0300

    prototype_

---
+++ Prototype/script.js
@@ -0,0 +1,28 @@
+//Crie uma função contrutora de pessoas
+//Deve conter nome, sobrenome e idade
+// Crie um método no protótipo que retorne o nome completo e idade da pessoa
+function Pessoa(nome, sobrenome, idade) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.idade = idade;
+}
+Pessoa.prototype.nomeCompleto = function() {
+    return `${this.nome} ${this.sobrenome} Idade: ${this.idade} anos.`;
+}
+const roberto = new Pessoa("roberto", "conrado", 28);
+
+//Liste os métodos acessados por dados criados com NodeList, HTMLCollection, Document
+Object.getOwnPropertyNames(NodeList.prototype)
+Object.getOwnPropertyNames(HTMLAllCollection.prototype)
+Object.getOwnPropertyNames(Document.prototype)
+
+//Liste os contrutores dos dados abaixo
+const li = document.querySelector('li');
+
+li; //HTMLLIElement
+li.click; //Function
+li.innerText; //String
+li.value; //Number
+li.hidden; //Boolean
+li.offsetLeft; //Number
+li.click(); //Undefined
