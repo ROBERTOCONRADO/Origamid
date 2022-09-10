795e2f6004bb20fb6757f333765e53153d45b32d
RobertoConrado <robertoconradorc@gmail.com>
2022-09-09 21:55:40 -0300

    reduce()

--- Objetos/Objetos-Funcao/script.js
+++ Objetos/Objetos-Funcao/script.js
@@ -1,12 +1,11 @@
-// Retorne a soma total de caracteres dos
-// parágrafos acima utilizando reduce
+// Retorne a soma total de caracteres dos parágrafos utilizando reduce
+const paragrafos = document.querySelectorAll('p');
+const totalCaracteres = Array.prototype.reduce.call(paragrafos, (acumulador, item) => {
+    return acumulador + item.innerText.length;
+}, 0);
+console.log(totalCaracteres);
+
+// Crie uma função que retorne novos elementos html, com os seguintes parâmetros tag, classe e conteudo.
 
 
-// Crie uma função que retorne novos elementos
-// html, com os seguintes parâmetros
-// tag, classe e conteudo.
-
-
-// Crie uma nova função utilizando a anterior como base
-// essa nova função deverá sempre criar h1 com a
-// classe titulo. Porém o parâmetro conteudo continuará dinâmico
+// Crie uma nova função utilizando a anterior como baseessa nova função deverá sempre criar h1 com a classe titulo. Porém o parâmetro conteudo continuará dinâmico
