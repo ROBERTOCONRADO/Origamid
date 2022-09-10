71970926a1d9f901ef1a31db83f4321bac7f04c4
RobertoConrado <robertoconradorc@gmail.com>
2022-09-09 22:06:16 -0300

    createElement

--- Objetos/Objetos-Funcao/script.js
+++ Objetos/Objetos-Funcao/script.js
@@ -6,6 +6,12 @@ const totalCaracteres = Array.prototype.reduce.call(paragrafos, (acumulador, ite
 console.log(totalCaracteres);
 
 // Crie uma função que retorne novos elementos html, com os seguintes parâmetros tag, classe e conteudo.
+function criarElemento(tag, classe, conteudo) {
+    const elemento = document.createElement(tag);
+    classe ? elemento.classList.add('classe') : '';
+    conteudo ? elemento.innerHTML = conteudo : null;
+    return elemento;
+}
+console.log(criarElemento('li', 'teste', 'Óla! Seja bem-vindo.'));
 
-
-// Crie uma nova função utilizando a anterior como baseessa nova função deverá sempre criar h1 com a classe titulo. Porém o parâmetro conteudo continuará dinâmico
+// Crie uma nova função utilizando a anterior como base essa nova função deverá sempre criar h1 com a classe titulo. Porém o parâmetro conteudo continuará dinâmico
