23396fa0c435d9b9017e29c75b5099a465ed0260
RobertoConrado <robertoconradorc@gmail.com>
2022-09-09 23:28:09 -0300

    getOwnPropertyNames()

--- Objetos/script3.js
+++ Objetos/script3.js
@@ -19,7 +19,12 @@ const configuracao = {
     width: 800,
     height: 600,
     background: '#333'
-  }
-  
-  // Liste o nome de todas as propriedades do protótipo de String e Array
-  
+}
+Object.freeze(configuracao);
+
+// Liste o nome de todas as propriedades do protótipo de String e Array
+console.log(Object.getOwnPropertyNames(String.prototype)); 
+// (50) ['length', 'constructor', 'anchor', 'at', 'big', 'blink', 'bold', 'charAt', 'charCodeAt', 'codePointAt', 'concat', 'endsWith', 'fontcolor', 'fontsize', 'fixed', 'includes', 'indexOf', 'italics', 'lastIndexOf', 'link', 'localeCompare', 'match', 'matchAll', 'normalize', 'padEnd', 'padStart', 'repeat', 'replace', 'replaceAll', 'search', 'slice', 'small', 'split', 'strike', 'sub', 'substr', 'substring', 'sup', 'startsWith', 'toString', 'trim', 'trimStart', 'trimLeft', 'trimEnd', 'trimRight', 'toLocaleLowerCase', 'toLocaleUpperCase', 'toLowerCase', 'toUpperCase', 'valueOf']
+
+console.log(Object.getOwnPropertyNames(Array.prototype));  
+// (36) ['length', 'constructor', 'at', 'concat', 'copyWithin', 'fill', 'find', 'findIndex', 'lastIndexOf', 'pop', 'push', 'reverse', 'shift', 'unshift', 'slice', 'sort', 'splice', 'includes', 'indexOf', 'join', 'keys', 'entries', 'values', 'forEach', 'filter', 'flat', 'flatMap', 'map', 'every', 'some', 'reduce', 'reduceRight', 'toLocaleString', 'toString', 'findLast', 'findLastIndex']
