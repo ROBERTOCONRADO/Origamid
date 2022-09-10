d2c8681dc4b9c6fc458c0430e16627ec033ddbf1
RobertoConrado <robertoconradorc@gmail.com>
2022-09-09 23:22:14 -0300

    ddefineProperties()

--- Objetos/script3.js
+++ Objetos/script3.js
@@ -1,6 +1,18 @@
 // Crie uma função que verifique corretamente o tipo de dado
+function verificarDado(dado) {
+    return Object.prototype.toString.call(dado);
+} 
+console.log(verificarDado('string'));
 
 // Crie um objeto quadrado com a propriedade lados e torne ela imutável
+const quadrado = {};
+Object.defineProperties(quadrado, {
+    lados: {
+        value: 4,
+        enumerable: false,
+    }
+});
+console.log(quadrado.lados); //<== {lados: 4}
 
 // Previna qualquer mudança no objeto abaixo
 const configuracao = {
