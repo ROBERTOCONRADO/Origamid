24ea27189949dec66a57ded4599fff6b4a951828
RobertoConrado <robertoconradorc@gmail.com>
2022-09-11 11:52:26 -0300

    modal.css

--- AnimaisFantasticos/js/import.js
+++ AnimaisFantasticos/js/import.js
@@ -3,7 +3,6 @@ import initAnimacaoScroll from './modules/scroll-animacao.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
 
-
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
