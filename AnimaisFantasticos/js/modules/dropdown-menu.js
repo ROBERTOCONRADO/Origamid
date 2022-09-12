5dac881833d9fddafa853c05a7fd18cc140c7b08
RobertoConrado <robertoconradorc@gmail.com>
2022-09-12 12:40:00 -0300

    dropDown-callback

--- AnimaisFantasticos/js/modules/dropdown-menu.js
+++ AnimaisFantasticos/js/modules/dropdown-menu.js
@@ -1,4 +1,30 @@
 export default function initDropDownMenu() {
-    const dropDown = document.querySelector('[data-dropdown]');
-    
+    const dropDownMenus = document.querySelectorAll('[data-dropdown]');
+
+    dropDownMenus.forEach(menu => {
+        ['touchstart', 'click'].forEach(userEvent => {
+            menu.addEventListener(userEvent, handleClick);
+        })
+    })
+
+    function handleClick(event) {
+        event.preventDefault();
+        this.classList.add('active');
+        outsideClick(this, () => {
+            this.classList.remove('active');
+        });
+    }
+
+    function outsideClick(element, callback) {
+        const html = document.documentElement;
+        html.addEventListener('click', handleOutsideClick);
+        function handleOutsideClick(event) {
+            if(!element.contains(event.target)) {
+                html.removeEventListener('click', handleOutsideClick);
+                callback();
+            }
+        }
+    }
 }
+
+
