4230ab310b32f0f47d21baa8d060f7db894d05f8
RobertoConrado <robertoconradorc@gmail.com>
2022-09-12 10:01:47 -0300

    appendChild

--- AnimaisFantasticos/js/modules/tooltip.js
+++ AnimaisFantasticos/js/modules/tooltip.js
@@ -1,8 +1,30 @@
 export default function initTooltip() {
-    const tooltip = document.querySelector('[data-tooltip]');
+    const tooltip = document.querySelectorAll('[data-tooltip]');
     console.log(tooltip);
 
+    tooltip.forEach((item) => {
+        item.addEventListener('mouseover', onMouseOver);
+    });
+    function onMouseOver(event) {
+        const tooltipBox = criarTooltipBox(this)
+        tooltipBox.style.top = event.pageY + 'px'
+        tooltipBox.style.left = event.pageX + 'px'
 
+        this.addEventListener('mouseleave', onMouseLeave);
+    }
+
+    onMouseLeave() {
+        tooltipBox.remove();
+    }
+    
+    function criarTooltipBox(element) {
+        const tooltipBox = document.createElement('div');
+        const text = element.getAttribute('aria-label');
+        tooltipBox.classList.add('tooltip');
+        tooltipBox.innerText = text;
+        document.body.appendChild(tooltipBox);
+        return tooltipBox;
+    }
 
 
 
