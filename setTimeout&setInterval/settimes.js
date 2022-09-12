f1697ef16a0d53bd42bae70f76a3187f7e10eda6
RobertoConrado <robertoconradorc@gmail.com>
2022-09-12 15:31:44 -0300

    cronometro

--- setTimeout&setInterval/settimes.js
+++ setTimeout&setInterval/settimes.js
@@ -6,5 +6,21 @@ function mudarClasse() {
 mudarClasse();
 
 //Crie um cronometro utilizando o setInterval. Deve ser possível iniciar, pausar e resetar (duplo clique no pausar)
+const iniciar = document.querySelector('.iniciar');
+const pausar = document.querySelector('.pausar');
+const tempo = document.querySelector('.tempo');
+
+iniciar.addEventListener('click', iniciarTempo);
+pausar.addEventListener('click', pausarTempo);
+pausar.addEventListener('dblclick', resetarTempo);
 
 
+function iniciarTempo() {
+    console.log('iniciou');
+}
+function pausarTempo() {
+    console.log('pausou');
+}
+function resetarTempo() {
+    console.log('reswetro');
+}
