ac65cc04ace9791868f67d6a55cabd0253f548b4
RobertoConrado <robertoconradorc@gmail.com>
2022-09-13 00:14:43 -0300

    numeros

--- AnimaisFantasticos/js/modules/menu-mobile.js
+++ AnimaisFantasticos/js/modules/menu-mobile.js
@@ -1,18 +1,18 @@
 import outsideClick from "./outsideclick.js"; 
 export default function initMenuMobile() {
-
+    const menuButton = document.querySelector('[data-menu="button"]');
+    const menuList = document.querySelector('[data-menu="list"]');
+    const eventos = ['click', 'touchstart'];
+    if(menuButton) {
+        function openMenu(event) {
+            menuList.classList.add('active');
+            menuButton.classList.add('active');
+            outsideClick(menuList, eventos, () => {
+                menuList.classList.remove('active');
+                menuButton.classList.remove('active');
+            })
+        }
+        eventos.forEach(evento => menuButton.addEventListener(evento, openMenu));
+    }
 }
 
-
-const menuButton = document.querySelector('[data-menu="button"]');
-const menuList = document.querySelector('[data-menu="list"]');
-const eventos = ['click', 'touchstart'];
-function openMenu(event) {
-    menuList.classList.add('active');
-    menuButton.classList.add('active');
-    outsideClick(menuList, eventos, () => {
-        menuList.classList.remove('active');
-        menuButton.classList.remove('active');
-    })
-}
-eventos.forEach(evento => menuButton.addEventListener(evento, openMenu));
