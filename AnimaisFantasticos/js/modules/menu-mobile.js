a7a6df6976cb20cd041a151511d271b995369467
RobertoConrado <robertoconradorc@gmail.com>
2022-09-13 00:05:45 -0300

    setTimeout

--- AnimaisFantasticos/js/modules/menu-mobile.js
+++ AnimaisFantasticos/js/modules/menu-mobile.js
@@ -6,11 +6,13 @@ export default function initMenuMobile() {
 
 const menuButton = document.querySelector('[data-menu="button"]');
 const menuList = document.querySelector('[data-menu="list"]');
-
+const eventos = ['click', 'touchstart'];
 function openMenu(event) {
     menuList.classList.add('active');
     menuButton.classList.add('active');
+    outsideClick(menuList, eventos, () => {
+        menuList.classList.remove('active');
+        menuButton.classList.remove('active');
+    })
 }
-
-['touchstart', 'click']
-menuButton.addEventListener('click', openMenu);
+eventos.forEach(evento => menuButton.addEventListener(evento, openMenu));
