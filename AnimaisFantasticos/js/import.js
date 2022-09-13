107eaed319cd0e0725f29422df8bc189d6a99f17
RobertoConrado <robertoconradorc@gmail.com>
2022-09-13 00:35:34 -0300

    numeros-animados

--- AnimaisFantasticos/js/import.js
+++ AnimaisFantasticos/js/import.js
@@ -6,6 +6,7 @@ import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initDropDownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
+import initAnimaNumeros from './modules/anima-numeros.js';
 
 initScrollSuave();
 initAnimacaoScroll();
@@ -15,3 +16,4 @@ initModal();
 initTooltip();
 initDropDownMenu();
 initMenuMobile();
+initAnimaNumeros();
