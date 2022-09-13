4dffcb65fba382ede490c7fe4294b77e3f43f33d
RobertoConrado <robertoconradorc@gmail.com>
2022-09-13 14:40:09 -0300

    Status

--- AnimaisFantasticos/js/modules/funcionamento.js
+++ AnimaisFantasticos/js/modules/funcionamento.js
@@ -1,21 +1,23 @@
 export default function initFuncionamento() {
+    const funcionamento = document.querySelector('[data-semana]');
+    
+    const diasSemana = funcionamento.dataset.semana.split(',').map(Number);
+    const horarioSemana = funcionamento.dataset.horario.split(',').map(Number);
+    
+    const dataAgora = new Date();
+    
+    const diaAtual = dataAgora.getDay();
+    const horarioAtual = dataAgora.getHours();  
+    
+    const abertoSemana = diasSemana.indexOf(diaAtual) !== -1;
+    const horarioAberto = (horarioAtual >= horarioSemana[0]  && horarioAtual < horarioSemana[1]);
+    
+    if(abertoSemana && horarioAberto) {
+        funcionamento.classList.add('aberto');
+    }
+}/*style.css 329*/
 
-}
 
-const funcionamento = document.querySelector('[data-semana]');
-
-const diasSemana = funcionamento.dataset.semana.split(',').map(Number);
-const horarioSemana = funcionamento.dataset.horario.split(',').map(Number);
-
-
-
-const dataAgora = new Date();
-
-const diaAtual = dataAgora.getDay();
-const horarioAtual = dataAgora.getHours();
-
-const abertoSemana = diasSemana.indexOf(diaAtual) !== -1;
-console.log(abertoSemana)
 
 
 
