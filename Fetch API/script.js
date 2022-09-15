07c7e7b3d82352c978f8b16651331978bd338cf3
RobertoConrado <robertoconradorc@gmail.com>
2022-09-14 23:37:07 -0300

    Gerador de piadas do ChuckNorris!

--- Fetch API/script.js
+++ Fetch API/script.js
@@ -29,8 +29,19 @@ function fetchBtc() {
         btcParagrafo.innerText = ('R$ ' + btcJson.BRL.buy).replace('.', ',');
     })
 }
-setInterval(fetchBtc, 1000 * 30);
+setInterval(fetchBtc, 1000 * 100);
 fetchBtc();
 
 
 // Utilizando a API https://api.chucknorris.io/jokes/random retorne uma piada randomica do chucknorris, toda vez que clicar em próxima
+const btnPiada = document.querySelector('.proxima');
+const piadaParagrafo = document.querySelector('.piada');
+
+function buscarPiada() {
+    fetch('https://api.chucknorris.io/jokes/random')
+    .then(r => r.json())
+    .then(piada => {
+        piadaParagrafo.innerText = piada.value;
+    })
+}
+btnPiada.addEventListener('click', buscarPiada);
