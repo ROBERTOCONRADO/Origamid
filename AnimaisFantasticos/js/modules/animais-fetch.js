af56cf11d16d05a524279398ad7220958ec275e9
RobertoConrado <robertoconradorc@gmail.com>
2022-09-15 12:36:45 -0300

    appendClild

--- AnimaisFantasticos/js/modules/animais-fetch.js
+++ AnimaisFantasticos/js/modules/animais-fetch.js
@@ -1,3 +1,23 @@
 export default function initAnimaisFetch() {
 
 }
+
+async function fetchAnimais(url) {
+    const animasiResponse = await fetch(url);
+    const animaisJSON = await animasiResponse.json();
+    const numerosGrid = document.querySelector('.numeros-grid');
+
+    animaisJSON.forEach(animal => {
+        const divAnimal = criarAnimal(animal);
+        numerosGrid.appendChild(divAnimal);
+    });
+}
+
+function criarAnimal(animal) {
+    const div = document.createElement('div');
+    div.classList.add('numero-animal');
+    div.innerHTML = `<h3>${animal.specie}</h3><span data-numero>${animal.total}</span>`;
+    return div;
+}
+
+fetchAnimais('/AnimaisFantasticos/js/animaisAPI/animaisapi.json');
