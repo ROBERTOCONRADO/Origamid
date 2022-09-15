0a7b59abb318694a7946310067aeb3338d1e8ee7
RobertoConrado <robertoconradorc@gmail.com>
2022-09-15 14:27:37 -0300

    import.js

--- AnimaisFantasticos/js/import.js
+++ AnimaisFantasticos/js/import.js
@@ -6,7 +6,6 @@ import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initDropDownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
-import initAnimaNumeros from './modules/anima-numeros.js';
 import initFuncionamento from './modules/funcionamento.js';
 import initAnimaisFetch from './modules/animais-fetch.js';
 
@@ -18,6 +17,5 @@ initModal();
 initTooltip();
 initDropDownMenu();
 initMenuMobile();
-initAnimaNumeros();
 initFuncionamento();
 initAnimaisFetch();
