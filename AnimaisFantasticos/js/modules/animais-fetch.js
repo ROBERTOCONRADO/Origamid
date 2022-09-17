46831394d6b6cab8511d081d02f885a75fc0c63d
RobertoConrado <robertoconradorc@gmail.com>
2022-09-17 13:22:31 -0300

    try

--- AnimaisFantasticos/js/modules/animais-fetch.js
+++ AnimaisFantasticos/js/modules/animais-fetch.js
@@ -2,15 +2,19 @@ import initAnimaNumeros from './anima-numeros.js';
 
 export default function initAnimaisFetch() {
     async function fetchAnimais(url) {
-        const animasiResponse = await fetch(url);
-        const animaisJSON = await animasiResponse.json();
-        const numerosGrid = document.querySelector('.numeros-grid');
-    
-        animaisJSON.forEach(animal => {
-            const divAnimal = criarAnimal(animal);
-            numerosGrid.appendChild(divAnimal);
-        });
-        initAnimaNumeros();
+        try {
+            const animasiResponse = await fetch(url);
+            const animaisJSON = await animasiResponse.json();
+            const numerosGrid = document.querySelector('.numeros-grid');
+        
+            animaisJSON.forEach(animal => {
+                const divAnimal = criarAnimal(animal);
+                numerosGrid.appendChild(divAnimal);
+            });
+            initAnimaNumeros();
+        } catch(erro) {
+            console.log(erro);
+        }
     }
     
     function criarAnimal(animal) {
--- AnimaisFantasticos/js/modules/fetch-BitCoin.js
+++ AnimaisFantasticos/js/modules/fetch-BitCoin.js
@@ -4,6 +4,8 @@ export default function initFetchBitcoin() {
     .then(bitcoin => {
         const btcPreco = document.querySelector('.valorBitcoin');
         btcPreco.innerText = (100 / bitcoin.BRL.sell).toFixed(4);
-    });
+    }).catch(erro => {
+        console.log(Error(erro));
+    })
 }
 
