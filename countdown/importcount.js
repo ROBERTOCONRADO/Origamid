12dc81710236ba50bb859542334e983e511aeba4
RobertoConrado <robertoconradorc@gmail.com>
2022-09-18 15:47:20 -0300

    stylesContagem

--- countdown/importcount.js
+++ countdown/importcount.js
@@ -5,7 +5,6 @@ const tempoParaoAnoNovo = new Countdown('31 December 2022 23:59:59 GMT-0300');
 const tempoParaOutubro = new Countdown('30 September 2022 23:59:59 GMT-0300');
 const tempoParaNovembro = new Countdown('31 October 2022 23:59:59 GMT-0300');
 
-
 // console.log(tempoParaOutubro.total);
 // console.log(tempoParaNovembro.total);
 // console.log(tempoParaoNatal.total);
@@ -17,13 +16,13 @@ const natal = document.querySelector('.natal')
 const anoNovo = document.querySelector('.anoNovo')
 
 setInterval(() => {
-    outubro.innerText = `${tempoParaOutubro.total.days} dias, ${tempoParaOutubro.total.hours} horas, ${tempoParaOutubro.total.minutes} minutos, ${tempoParaOutubro.total.seconds} segundos`
+    outubro.innerText = `${tempoParaOutubro.total.days}: ${tempoParaOutubro.total.hours}: ${tempoParaOutubro.total.minutes}: ${tempoParaOutubro.total.seconds}`
 
-    novembro.innerText = `${tempoParaNovembro.total.days} dias, ${tempoParaNovembro.total.hours} horas, ${tempoParaNovembro.total.minutes} minutos, ${tempoParaNovembro.total.seconds} segundos`
+    novembro.innerText = `${tempoParaNovembro.total.days}: ${tempoParaNovembro.total.hours}: ${tempoParaNovembro.total.minutes}: ${tempoParaNovembro.total.seconds}`
 
-    natal.innerText = `${tempoParaoNatal.total.days} dias, ${tempoParaoNatal.total.hours} horas, ${tempoParaoNatal.total.minutes} minutos, ${tempoParaoNatal.total.seconds} segundos`
+    natal.innerText = `${tempoParaoNatal.total.days}: ${tempoParaoNatal.total.hours}: ${tempoParaoNatal.total.minutes}: ${tempoParaoNatal.total.seconds}`
     
-    anoNovo.innerText = `${tempoParaoAnoNovo.total.days} dias, ${tempoParaoAnoNovo.total.hours} horas, ${tempoParaoAnoNovo.total.minutes} minutos, ${tempoParaoAnoNovo.total.seconds} segundos`
+    anoNovo.innerText = `${tempoParaoAnoNovo.total.days}: ${tempoParaoAnoNovo.total.hours}: ${tempoParaoAnoNovo.total.minutes}: ${tempoParaoAnoNovo.total.seconds}`
 }, 1000);
 
 
