c046729d5fb9d7c354bb5a5b848ce219d7cd5fb2
RobertoConrado <robertoconradorc@gmail.com>
2022-09-18 14:43:42 -0300

    TempoParaNatal

--- countdown/importcount.js
+++ countdown/importcount.js
@@ -11,4 +11,25 @@ console.log(tempoParaNovembro.total);
 console.log(tempoParaoNatal.total);
 console.log(tempoParaoAnoNovo.total);
 
+const outubro = document.querySelector('.outubro')
+const novembro = document.querySelector('.novembro')
+const natal = document.querySelector('.natal')
+const anoNovo = document.querySelector('.anoNovo')
+
+
+console.log(outubro[0])
+outubro.innerText = `${tempoParaOutubro.total.days} dias, ${tempoParaOutubro.total.hours} horas, ${tempoParaOutubro.total.minutes} minutos, ${tempoParaOutubro.total.seconds} segundos`
+
+
+novembro.innerText = `${tempoParaNovembro.total.days} dias, ${tempoParaNovembro.total.hours} horas, ${tempoParaNovembro.total.minutes} minutos, ${tempoParaNovembro.total.seconds} segundos`
+
+
+natal.innerText = `${tempoParaoNatal.total.days} dias, ${tempoParaoNatal.total.hours} horas, ${tempoParaoNatal.total.minutes} minutos, ${tempoParaoNatal.total.seconds} segundos`
+
+
+anoNovo.innerText = `${tempoParaoAnoNovo.total.days} dias, ${tempoParaoAnoNovo.total.hours} horas, ${tempoParaoAnoNovo.total.minutes} minutos, ${tempoParaoAnoNovo.total.seconds} segundos`
+
+
+
+
 
