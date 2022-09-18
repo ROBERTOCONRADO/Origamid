aff5fe273e4088a578e0154e22bea2a39c32fad3
RobertoConrado <robertoconradorc@gmail.com>
2022-09-17 22:33:42 -0300

    countdown_function

--- countdown/countdown.js
+++ countdown/countdown.js
@@ -1,5 +1,17 @@
-export default function Countdown() {
+export default class Countdown {
     constructor(futureDate) {
         this.futureDate = futureDate;
+    }
+    get _actualDate() {
+        return new Date();
+    }
+    get _futureDate() {
+        return new Date(this.futureDate);
+    }
+    get _timeStampDiff() {
+        return this._futureDate.getTime() - this._actualDate.getTime();
+    }
+    get days() {
+        return Math.floor(this._timeStampDiff / (24 * 60 * 60 * 1000));
     }
 }
--- countdown/importcount.js
+++ countdown/importcount.js
@@ -1,3 +1,6 @@
 import Countdown from './countdown.js';
 
 const diasParaoNatal = new Countdown('24 December 2022 23:59:59 GMT-0300');
+
+
+console.log(diasParaoNatal.days);
