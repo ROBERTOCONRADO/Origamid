71416bbc6049bc17254bc7a01eb3e918a260c45e
RobertoConrado <robertoconradorc@gmail.com>
2022-09-19 14:28:32 -0300

    {destructuring}

--- destructuring/script.js
+++ destructuring/script.js
@@ -8,4 +8,17 @@ console.log(backgroundColor);
 console.log(color);
 console.log(margin);
 
+//Troque os valores das variáveis abaixo 
+let cursoAtivo = 'JavaScript';
+let cursoInativo = 'HTML';
+
+//Corrija o erro abaixo
+const cachorro = {
+  nome: 'bob',
+  raca: 'labrador',
+  cor: 'amarelo'
+}
+
+const {cor: corDoCachorro} = cachorro;
+
 
