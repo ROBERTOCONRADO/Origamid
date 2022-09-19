fa9ca27a1e20ae9cc12bd22a24d575006a47bc33
RobertoConrado <robertoconradorc@gmail.com>
2022-09-19 15:15:43 -0300

    ...fruts

---
+++ rest&spread/script.js
@@ -0,0 +1,29 @@
+//Reescreva a função utilizando valores iniciais de parâmetros com ES6
+function createButton(background, color) {
+    background = background || 'blue';
+    if(color === undefined) {
+        color = 'red';
+    }
+    const buttonElement = document.createElement('button');
+    buttonElement.style.background = background;
+    buttonElement.style.color = color;
+    return buttonElement;
+}
+const redButton = createButton();
+
+//Com ES6
+function createButton(background = 'blue', color = 'red') {
+    const buttonElement = document.createElement('button');
+    buttonElement.style.background = background;
+    buttonElement.style.color = color;
+    return buttonElement;
+}
+const Button = createButton();
+
+
+//Utilize o método push para inserir as frutas ao final de comidas
+const frutas = ['banana', 'maça', 'pêra'];
+const comidas = ['pizza', 'lanche', 'batata'];
+
+comidas.push(...frutas);
+console.log(comidas);//<== 'pizza', 'lanche', 'batata', 'banana', 'maça', 'pêra'
