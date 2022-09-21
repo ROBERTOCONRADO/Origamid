932a7469dff9122a5c6bc948a310bc3605235e6d
RobertoConrado <robertoconradorc@gmail.com>
2022-09-20 23:14:08 -0300

    limparCPFfunction

--- regexp-limparCPF/script.js
+++ regexp-limparCPF/script.js
@@ -1,10 +1,29 @@
-const cpfs = document.querySelectorAll('.cpf li');
+const cpfsLista = document.querySelectorAll('.cpf li');
 
-function elementsInnerText(e) {
-    const arrayElements = Array.from(e);
-    return arrayElements.map(element => {
-        return element.innerText;
-    });
+const elementsInnerText = ([...e]) => {
+    return e.map(element => element.innerText);
 }
 
-console.log(elementsInnerText(cpfs));
+const limparCPF = (cpf) => {
+    return cpf.replace(/\D/g, '');
+}
+
+const  construirCPF = (cpf) => {
+    return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g, '$1.$2.$3-$4');
+}
+
+const formatarCPFS = (cpfs) => {
+    return cpfs.map(limparCPF).map(construirCPF);
+}
+
+const substituirCPFS = (cpfsElements) => {
+    const cpfs = elementsInnerText(cpfsElements);
+    const cpfsFormatados = formatarCPFS(cpfs);
+
+    cpfsElements.forEach((element, index) => {
+       element.innerText =  cpfsFormatados[index];
+    })
+    console.log(cpfsFormatados);
+}
+
+substituirCPFS(cpfsLista);
