9d893230666440c01e97389021c8943381692a77
RobertoConrado <robertoconradorc@gmail.com>
2022-09-21 14:55:09 -0300

    formatarcpf

--- validacao_formulario/script.js
+++ validacao_formulario/script.js
@@ -2,7 +2,4 @@ import Validar from './validar.js';
 
 
 const cpf = document.querySelector('#cpf');
-const validar = new Validar(cpf);
-
-
-console.log(validar.formatar('111.333.22207'));
+const validar = new Validar(cpf).iniciar();
--- validacao_formulario/validar.js
+++ validacao_formulario/validar.js
@@ -12,5 +12,25 @@ export default class Validar {
         const cpfLimpo = this.limpar(cpf);
         return this.construir(cpfLimpo);
     }
+    validar(cpf) {
+        const matchCpf = cpf.match(/(?:\d{3}[-.\s]?){3}\d{2}/g);
+        return (matchCpf && matchCpf[0] === cpf);
+    }
+    validarMudanca(cpfElement) {
+        if(this.validar(cpfElement.value)) {
+            cpfElement.value = this.formatar(cpfElement.value);
+        }else{
+
+        }
+    }
+    adicionarEvento() {
+        this.element.addEventListener('change', () => {
+            this.validarMudanca(this.element);
+        });
+    }
+    iniciar() {
+        this.adicionarEvento();
+        return this;
+    }
 }
 
